36c8c75599e474fe32b89dd77ae110e3906bd8a3
Shivam Upadhyay <68780771+shivamu9@users.noreply.github.com>
2026-01-12 18:04:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const http = require('http'); // Import the http module
-const port = 8080; // Define the port number
+const http = require('node:http'); // Import the http module
 
+const hostname = '127.0.0.1'; // Localhost
+const port = 8000; // The desired port
+
+// Create an HTTP server that handles requests and returns a response
 const server = http.createServer((req, res) => {
-  res.statusCode = 200; // Set the HTTP status code to OK
-  res.setHeader('Content-Type', 'text/plain'); // Set the response header
-  res.end('Hello World\n'); // Send the response body and end the connection
+  res.statusCode = 200; // Set the response status code to OK
+  res.setHeader('Content-Type', 'text/plain'); // Set the content type
+  res.end('Hello World\n'); // Send the response body
 });
 
-server.listen(port, () => {
-  // The server starts listening on the specified port
-  console.log(`Server running at http://localhost:${port}/`);
+// Start the server and make it listen on the specified port and hostname
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
